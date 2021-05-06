08babf629c97190956c13b70a767abdaa9c7a143
wxw xxx <m202076583@hust.edu.cn>
2021-05-06 15:37:00 +0800

    初始化cpp项目

---
+++ 001.hello_world/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: Do not edit
+ * @Author: linkenzone
+ * @Date: 2021-04-28 17:29:04
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 1;
+    cout << "Hello World"; // 输出 Hello World
+    return 0;
+}
