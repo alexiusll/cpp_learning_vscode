2d12f1fc2a3be3c545101bef217044bc48c20675
Alexiusll <linkenzone@163.com>
2021-09-10 16:31:59 +0800

    更新文件格式

--- clear_cache.bat
+++ clear_cache.bat
@@ -1,9 +1,9 @@
 @echo off
-echo 清理临时文件中...
+echo clean...
 del %~dp0*.exe /q /s
 del %~dp0tempCodeRunnerFile.c /q /s
 del %~dp0a.out /q /s
 del %~dp0*.o /q /s
-echo 清理文件结束...按任意键退出
+echo end...press
 pause >nul
 exit
