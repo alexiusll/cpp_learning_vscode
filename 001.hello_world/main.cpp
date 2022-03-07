6990d4161dc896ae9d7dd7f8338791e99f3448fe
Alexiusll <linkenzone@163.com>
2022-03-07 10:44:59 +0800

    更新

--- 001.hello_world/main.cpp
+++ 001.hello_world/main.cpp
@@ -5,11 +5,43 @@
  */
 
 #include <iostream>
+#include <math.h>
+#include <vector>
+
+
 using namespace std;
 
-int main()
-{
-    int x = 1;
-    cout << "Hello World"; // 输出 Hello World
-    return 0;
+// vector<int> countBits(int n);
+
+// vector<int> countBits(int n) {
+//   vector<int> res;
+
+//   for (int i = 0; i <= n; i++) {
+//     int ans = 0;
+//     int temp = i;
+
+//     while (temp != 0) {
+//       int m = temp & 1;
+//       if (m == 1) {
+//         ans++;
+//       }
+//       temp = temp >> 1;
+//     }
+//     res.push_back(ans);
+//   }
+
+//   return res;
+// }
+
+int main() {
+  // int x = 1;
+  // cout << "Hello World"; // 输出 Hello World
+
+  cout << 1.00110011001100110011010 * pow(2, -2) << endl; // 输出 Hello World
+
+  cout << 1.001110 * pow(2, 6) << endl; // 输出 Hello World
+
+  // vector<int> res = countBits(2);
+
+  return 0;
 }
---
+++ 007.cpp_3.2/string.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Author: linkenzone
+ * @Date: 2021-11-24 15:54:35
+ * @Descripttion: Do not edit
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // string s;
+    // cin >> s;
+    // cout << s << endl;
+
+    // string s1, s2;
+    // cin >> s1 >> s2;
+    // cout << s1 << s2 << endl;
+
+    // string word;
+    // while(cin >> word)12
+    //     cout << word << endl;
+
+    // string line;
+    // while (getline(cin,line))
+    //     cout << line << endl;
+
+    // string str = "Hello";
+    // string phrase = "Hello World";
+    // string slang = "Hiya";
+
+    string str = "some string";
+    for (auto c : str)
+        cout << c << endl;
+
+    
+    
+
+    return 0;
+}
