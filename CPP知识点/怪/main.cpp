338b539094929ffe19da26fea7869311f24692aa
linkenzone <linkenzone@163.com>
2024-07-03 11:30:13 +0800

    更新

---
+++ CPP知识点/怪/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * @Author: linkenzone
+ * @Date: 2023-08-25 16:30:35
+ * @Descripttion: Do not edit
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int f(int a)
+{
+    return a = a * a;
+    // return b;
+}
+
+int main()
+{
+    const char *str = "/root/wzh/OpenCL_CTS/build/test_conformance/spirv_new/test_spirv_new";
+    printf("%d", strlen(str)); 
+    return 0;
+}
